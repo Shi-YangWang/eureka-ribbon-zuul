2d2d7088cd3ff3bc31af6bf0d94a811a74cee312
MariaLikesFish <ftracker1@biorun.com>
2020-05-06 19:12:46 +0800

    20200506CX

--- bunny-parent/eureka-consumer/src/main/java/com/bunny/consumer/controller/LoadBalanceController.java
+++ bunny-parent/eureka-consumer/src/main/java/com/bunny/consumer/controller/LoadBalanceController.java
@@ -1,7 +1,9 @@
 package com.bunny.consumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,12 @@ import org.springframework.web.client.RestTemplate;
 public class LoadBalanceController {
 
     @Autowired
+    private LoadBalancerClient loadBalancer;
+
+    @Autowired
     private RestTemplate restTemplate;
 
-    @LoadBalanced
+//    @LoadBalanced
     @Bean
     public RestTemplate rest() {
         return new RestTemplate();
@@ -24,6 +29,8 @@ public class LoadBalanceController {
     @RequestMapping(path = "/rest/query", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public String queryAnimal() {
-        return restTemplate.getForObject("http://service-provider/rest/query", String.class);
+        ServiceInstance instance = loadBalancer.choose("service-provider");
+        System.out.println(instance.getUri());
+        return restTemplate.getForObject(instance.getUri().toString() + "/rest/query", String.class);
     }
 }
